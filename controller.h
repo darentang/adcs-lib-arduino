964fb74df9db974846efcbe3d69fc136f6c48618
Daren Tang <daren1998tang@gmail.com>
2021-06-04 09:33:57 +1000

    Save work

--- controller.h
+++ controller.h
@@ -3,8 +3,11 @@
 
 #include "util.h"
 
-const float K_P = -0.4f/4000.0f;
-const float K_D = 2.0f/300.0f;
+//const float K_P = -0.4f/4000.0f;
+//const float K_D = 2.0f/300.0f;
+
+const float K_P = -300.0f;
+const float K_D = 600.0f;
 
 
 extern void controller(Vec3* T_c, Quaternion q_est, Quaternion q_ref, Quaternion omega_est);
